88a0ee6c5a05974cb020ce64c1dda9c2a74c7fa5
Simon <u7574421@anu.edu.au>
2023-09-22 11:07:34 +1000

    android project created.

---
+++ items/app/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void logIn(View view){
+        TextView userName=findViewById(R.id.userName);
+        TextView passWord=findViewById(R.id.passWord);
+        String userName1="comp2100@anu.edu.au";
+        String userName2="comp6442@anu.edu.au";
+        String passWord1="comp2100";
+        String passWord2="comp6442";
+        if((userName.getText().toString().equals(userName1)&&passWord.getText().toString().equals(passWord1))||(userName.getText().toString().equals(userName2)&&passWord.getText().toString().equals(passWord2))){
+
+        }else {
+            Toast toast=Toast.makeText(getApplicationContext(),"Invalid password, please try again!",Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+}
