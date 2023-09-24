e63bd420d0bf948f76d34f5fae599e0f4e1d3a2b
Simon <u7574421@anu.edu.au>
2023-09-24 16:44:16 +1000

    UI modified.

--- items/app/app/src/main/java/com/example/myapplication/GameDetails.java
+++ items/app/app/src/main/java/com/example/myapplication/GameDetails.java
@@ -1,13 +1,25 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GameDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent=getIntent();
         setContentView(R.layout.activity_game_details);
+        TextView name=findViewById(R.id.detailName);
+        TextView publisher=findViewById(R.id.detailPublisher);
+        TextView year=findViewById(R.id.detailYear);
+        TextView price=findViewById(R.id.detailPrice);
+        name.setText(intent.getStringExtra("name"));
+        publisher.setText(intent.getStringExtra("publisher"));
+        year.setText(Integer.toString(intent.getIntExtra("year",0)));
+        price.setText(Integer.toString(intent.getIntExtra("price",0)));
     }
 }
--- items/app/app/src/main/java/com/example/myapplication/GameList.java
+++ items/app/app/src/main/java/com/example/myapplication/GameList.java
@@ -2,9 +2,16 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -19,5 +26,17 @@ public class GameList extends AppCompatActivity {
         games=JsonParser.parseJsonFromAssets(this,"data.json");
         adapter=new GameAdapter(this,games);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent=new Intent(getApplicationContext(),GameDetails.class);
+                intent.putExtra("name", games.get(position).getName());
+                intent.putExtra("publisher", games.get(position).getProducer());
+                intent.putExtra("year", games.get(position).getYear());
+                intent.putExtra("price", games.get(position).getPrice());
+                startActivity(intent);
+            }
+        });
     }
+
 }
