41ea61d049979bea3726e8043674f43e8bf16e98
Zehua Liu <u7544341@anu.edu.au>
2023-09-24 16:45:39 +1000

    tokenizer etc.

--- items/app/app/src/main/java/com/example/myapplication/GameList.java
+++ items/app/app/src/main/java/com/example/myapplication/GameList.java
@@ -3,7 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -11,13 +13,34 @@ import java.util.ArrayList;
 public class GameList extends AppCompatActivity {
     GameAdapter adapter;
     public ArrayList<Game> games;
+    ArrayList<Game> originalGames;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_list);
         ListView listView=findViewById(R.id.gameList);
         games=JsonParser.parseJsonFromAssets(this,"data.json");
+        originalGames = new ArrayList<>(games);
         adapter=new GameAdapter(this,games);
         listView.setAdapter(adapter);
+    }
+    public void performSearch(View view) {
+        System.out.println("Clicked");
+        EditText searchEditText = findViewById(R.id.serachContent);
+        System.out.println(searchEditText);
+        String criteria = searchEditText.getText().toString();
+
+        Tokenizer tokenizer = new Tokenizer(criteria);
+
+        ArrayList<Game> matchedGames = new ArrayList<>();
+        for (Game game : originalGames) {
+            if (tokenizer.matches(game)) {
+                matchedGames.add(game);
+            }
+        }
+
+        games.clear();
+        games.addAll(matchedGames);
+        adapter.notifyDataSetChanged();
     }
 }
